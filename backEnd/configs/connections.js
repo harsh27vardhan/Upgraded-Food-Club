63d2e10b344573ad1144f93b89bfe82bfa7a4776
Harsh Vardhan <reachvardhanharsh9@gmail.com>
2024-12-21 01:40:38 +0530

    Added the atlas link in the connectionUrl of the mongoose

--- backEnd/configs/connections.js
+++ backEnd/configs/connections.js
@@ -1,12 +1,12 @@
-const mongooose = require("mongoose");
+const mongoose = require("mongoose");
 
 exports.connectMongoDB = (databaseName = "") => {
-  const connectionUrl = `${process.env.MONGODB_URL}/${
-    process.env.MONGODB_DB || databaseName
-  }`;
-  //   const connectionUrl = `mongodb+srv://harsh27vardhan:<db_password>@foodclubcluster.8pw3q.mongodb.net/?retryWrites=true&w=majority&appName=foodClubCluster`;
+  // const connectionUrl = `${process.env.MONGODB_URL}/${
+  //   process.env.MONGODB_DB || databaseName
+  // }`;
+  const connectionUrl = `mongodb+srv://harsh27vardhan:harshjitestmode@foodclubcluster.8pw3q.mongodb.net/foodclubdb?retryWrites=true&w=majority&appName=foodClubCluster`;
   console.log(connectionUrl);
-  mongooose
+  mongoose
     .connect(connectionUrl)
     .then(() => {
       console.log("Connected to MongoDB");
